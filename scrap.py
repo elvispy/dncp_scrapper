3e751ee77759451c331ce13a1c0c69e01cbdc871
Hernan Galeano Morel <hernanmorel@gmail.com>
2019-08-24 18:26:26 -0400

    Ir a siguiente página

--- scrap.py
+++ scrap.py
@@ -13,7 +13,7 @@ from selenium import webdriver
 #from random import randint
 import pandas as pd
 #import numpy
-#import re
+import re
 import os
 #import time
 
@@ -40,8 +40,8 @@ driver.get(web_url)
 
 sleep(randint(5,10))
 
-convocante = 'Municipalidad de Ciudad del Este'
-
+#convocante = 'Municipalidad de Ciudad del Este'
+convocante = 'Municipalidad de Presidente Franco'
 buscar_licitacion.buscar_licitacion(convocante, driver)
 
 solo_contratos = []
@@ -140,3 +140,36 @@ for i, etapa in enumerate(etapas_licit):
 
 df = pd.DataFrame(solo_contratos, columns=['id_licitacion', 'fecha_firma_contrato', 'num_contrato', 'nombre_empresa', 'ruc_empresa', 'monto_adjudicado', 'titulo_contrato', 'municipio', 'codigo_contratacion', 'contrato_download'])
 df.to_csv(os.getcwd() + '\\solo_contratos.csv', encoding='utf-8',index=False)
+
+#hacer click en siguiente "cantidad de veces"
+existe_siguiente_pagina = True
+while existe_siguiente_pagina == True:
+    try:
+        xp_ul_paginas = '//*[@id="licitaciones"]/div[2]/div/div[2]/div/ul'
+        uls = driver.find_element_by_xpath(xp_ul_paginas)
+        lis = uls.find_elements_by_tag_name("li")
+        
+        pagina_activa = uls.find_element_by_class_name("active").text #da la página activa
+        pagina_activa = re.search("\d+", pagina_activa).group(0)
+        
+        ultima_pagina = lis[-2].text
+        ultima_pagina = re.search("\d+", ultima_pagina).group(0)
+        
+        if pagina_activa != lis[-2].text:
+            lis[-1].find_element_by_tag_name("a").click() #hacer click en siguiente pagina
+            sleep(3)
+            print('La página activa es: ' + pagina_activa)
+            print('La última página es: ' + ultima_pagina)
+            existe_siguiente_pagina = True
+    except:
+        existe_siguiente_pagina = False
+        pass
+
+
+
+    
+#lis = driver.find_elements_by_xpath('//*[@id="licitaciones"]/div[2]/div/div[2]/div/ul/li')
+
+#cantidad = len(lis) - 3 #Cantidad de veces para hacer click en siguiente
+#for li in lis:
+#    print(li.text)
