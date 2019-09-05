6931079d15af14bbb1a4b2d1d9ee55bd162d5d8b
elvispy <elvisavfc65@gmail.com>
2019-09-04 20:23:14 -0400

    added some functionalities

--- main.py
+++ main.py
@@ -6,21 +6,32 @@ import formatt
 
 import savetojson
 
-year = 2015
-
+year = 2016
 municipio = 'Hernandarias'
+scrap_var = True #Want to scrap th website?
+up_to_cloud = False #Want to upload the data to drive?
 
-def main():
+def main(scrap_var = True, up_to_cloud = True):
 
     #The check if there is an error here first (It's likely that that's the case)
     xlstodrive.checkcredentials()
 
-    datos = scrap.main(municipio, year)
-
-    savetojson.main(datos)
     
-    xlstodrive.main(datos, year)
+    if scrap_var:
+        datos = scrap.main(municipio, year)
+        savetojson.main(datos)
+
+    else:
+
+        import json
+        file = open('data.json', 'rb')
+        datos = json.load(file)
+        datos = list(datos.values())
+
+        
+    if up_to_cloud:
+        xlstodrive.main(datos, year)
 
 if __name__ == '__main__':
 
-    main()
+    main(scrap_var, up_to_cloud)
